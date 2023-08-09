e3786d1bdda6f540fcb4a765d8c70174e987d0b1
x2y2-r2 <x2y2r2@pm.me>
2023-08-09 23:49:12 +0200

    Lekcja9

---
+++ src/Kot.java
@@ -0,0 +1,12 @@
+public class Kot {
+    String imie;
+    int wiek;
+
+    public void miaucz() {
+        System.out.println("Miauuuu!!");
+    }
+
+    public void przedstawSie() {
+        System.out.println("Cześć, jestem " + imie + ", mam " + wiek + " lat.");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Babka kociaBabka = new Babka();
+        kociaBabka.imie = "Kicia";
+        System.out.println(kociaBabka.imie);
+
+        Kot mruczek = new Kot();
+        mruczek.imie = "Mruczek";
+        mruczek.wiek = 4;
+
+        System.out.println(mruczek.imie);
+        mruczek.miaucz();
+        mruczek.przedstawSie();
+
+
+
+    }
+}
